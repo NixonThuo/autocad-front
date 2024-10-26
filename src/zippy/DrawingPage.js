3ee162951bb22d8bfefcaf06df10c6ab54d2129b
nixon.thuo@ngenx.io <nixon.thuo@ngenx.io>
2024-10-26 12:05:32 +0300

    this is excellent

--- src/zippy/DrawingPage.js
+++ src/zippy/DrawingPage.js
@@ -1,11 +1,17 @@
 // src/zippy/AboutPage.js
 
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function DrawingPage() {
+
+  library.add(faCheckSquare, faCoffee)
+
   return (
     <div style={{ textAlign: 'center', padding: '2rem' }}>
-      <h1>Drawing Page Here</h1>
+      <h1>Drawing Page Here <FontAwesomeIcon icon="check-square" /></h1>
       <p>This is where we will start designing.</p>
     </div>
   );
